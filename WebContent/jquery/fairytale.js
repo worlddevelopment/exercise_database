0e978d97917d49366910880549b8a45a53208b50
Jan R.I. B.F. v.W. v.Z. (faerietree) <radagast@ciry.at>
2017-03-07 22:29:36 +0100

    Add fairytale function to add or update query string, URL location.

--- WebContent/jquery/fairytale.js
+++ WebContent/jquery/fairytale.js
@@ -41,3 +41,33 @@
 	function removeEvent(el, e, f, mode) {
 	    return rmEvent(el, e, f, mode);
 	}
+	
+	// http://stackoverflow.com/questions/5999118/add-or-update-query-string-parameter
+	function UpdateQueryString(key, value, url) {
+	    if (!url) url = window.location.href;
+	    var re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi");
+
+	    if (re.test(url)) {
+	        if (typeof value !== 'undefined' && value !== null)
+	            return url.replace(re, '$1' + key + "=" + value + '$2$3');
+	        else {
+	            var hash = url.split('#');
+	            url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
+	            if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+	                url += '#' + hash[1];
+	            return url;
+	        }
+	    }
+	    else {
+	        if (typeof value !== 'undefined' && value !== null) {
+	            var separator = url.indexOf('?') !== -1 ? '&' : '?',
+	                hash = url.split('#');
+	            url = hash[0] + separator + key + '=' + value;
+	            if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+	                url += '#' + hash[1];
+	            return url;
+	        }
+	        else
+	            return url;
+	    }
+	}
