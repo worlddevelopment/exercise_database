2e77946377b5567c147f733521077ac4d8f1e544
Jan R.I. B.F. v.W. v.Z. (faerietree) <radagast@ciry.at>
2017-03-09 21:41:05 +0100

    LDAP: Connection: Style: white space,indent,comments. JavaDoc

--- src/db/LdapConnection.java
+++ src/db/LdapConnection.java
@@ -13,119 +13,114 @@ import javax.naming.ldap.InitialLdapContext;
 
 
 
-public class LdapConnection{
-    final String searchFilter = "cn=mail";
-	  final String searchBase = "ou=people,o=uni-wuerzburg";
+public class LdapConnection {
+	final String searchFilter = "cn=mail";
+	final String searchBase = "ou=people,o=uni-wuerzburg";
 
-	    
+
 
 	/**
-	 * This method is used to search an entry in LDAP using Novell API
+	 * Search an entry in LDAP using Novell API.
+	 *
 	 * @param dirContext
 	 * @param searchFilter
 	 * @param searchBase
 	 * @return {@link Boolean}
 	 * @throws NamingException
 	 */
-	private static final boolean getSearchResult(final DirContext dirContext, 
-	        final String searchFilter, 
-	        final String searchBase) throws NamingException{
-	    
-	    boolean retVal = false;
-	    
-	    final SearchControls constraints = new SearchControls();
-	   // constraints.setSearchScope(LDAPConstants.SUBTREE_SCOPE);
-	    
-	    System.out.println("** Search Starts : "  + System.currentTimeMillis());
-	    final NamingEnumeration<SearchResult> searchResults = dirContext.search(searchBase,searchFilter,constraints);
-	    System.out.println("** Search Ends : "  + System.currentTimeMillis());
-	    
-	    if(searchResults != null && searchResults.hasMore()){
-	        retVal= true;
-	        // For Example , displayed attribute values
-	       
-	        final SearchResult searchResult = (SearchResult)searchResults.next();
-	        displayAttributes(searchResult.getAttributes());
-	    }
+	private static final boolean getSearchResult(final DirContext dirContext,
+			final String searchFilter, final String searchBase)
+			throws NamingException{
 
-	    return retVal;
+		boolean retVal = false;
+
+		final SearchControls constraints = new SearchControls();
+		//constraints.setSearchScope(LDAPConstants.SUBTREE_SCOPE);
+
+		System.out.println("** Search Starts : "  + System.currentTimeMillis());
+		final NamingEnumeration<SearchResult> searchResults = dirContext.search(searchBase,searchFilter,constraints);
+		System.out.println("** Search Ends : "  + System.currentTimeMillis());
+
+		if (searchResults != null && searchResults.hasMore()){
+			retVal= true;
+			// For Example displayed attribute values
+			final SearchResult searchResult = (SearchResult)searchResults.next();
+			displayAttributes(searchResult.getAttributes());
+		}
+
+		return retVal;
 	}
+
+
+
 	/**
-	 * This method used to display the Attribute Values
+	 * Display attribute Values.
+	 *
 	 * @param attributes
 	 * @throws NamingException
 	 */
-	private static void displayAttributes(final Attributes attributes) throws NamingException{
-	    
-	    if (attributes == null) {
-	        System.out.println("*** No attributes ***");
-	    } 
-	    else {
-	        for (NamingEnumeration<? extends Attribute> enums = attributes.getAll(); enums.hasMore();) {
-	            final Attribute attribute = (Attribute)enums.next();
-	            System.out.println();
-	           System.out.println("Id :" + attribute.getID() + " " + attribute.get());
-	            if(attribute.getID().equals("fullName")){
-	            	System.out.println("Deine name :"  + attribute.get());
-	            }
-	            //attribute.get();
-//	            for (NamingEnumeration<?> namingEnu = attribute.getAll();namingEnu.hasMore();)
-//	                System.out.println("\t        = " + namingEnu.next());
-       }
+	private static void displayAttributes(final Attributes attributes)
+			throws NamingException {
 
-	    }
-	  
-}
+		if (attributes == null) {
+			System.out.println("*** No attributes ***");
+		}
+		else {
+			for (NamingEnumeration<? extends Attribute> enums = attributes.getAll(); enums.hasMore();) {
+				final Attribute attribute = (Attribute)enums.next();
+				System.out.println();
+				System.out.println("Id :" + attribute.getID() + " " + attribute.get());
+				if (attribute.getID().equals("fullName")){
+					System.out.println("Deine name :"  + attribute.get());
+				}
+				//attribute.get();
+//				for (NamingEnumeration<?> namingEnu = attribute.getAll();namingEnu.hasMore();)
+//					System.out.println("\t		= " + namingEnu.next());
+			}
+
+		}
+
+	}
+
+
+
+	/**
+	 * Main, entry point for e.g. testing.
+	 *
+	 * @param args Parameters given e.g. per command line.
+	 */
 	public static void main(String[] args) {
-//		Properties env = new Properties();
-//	      env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-//	      env.put(Context.PROVIDER_URL, "ldap://adressbuch.uni-wuerzburg.de:389");//ldap://adressbuch.uni-wuerzburg.de:389/ou=people,o=uni-wuerzburg??one?(objectClass=*)
-//	      //env.put(Context.REFERRAL, "folow");
-//	      env.put(Context.SECURITY_AUTHENTICATION, "none");
-//	      //env.put(Context.SECURITY_PRINCIPAL, "ou=people,o=uni-wuerzburg");
-//	      //env.put(Context.SECURITY_CREDENTIALS, "artjom84");
-//	      try {
-//	    	  
-//	          DirContext ctx = new InitialLdapContext(env, null);
-//	          //System.out.println( ctx.createSubcontext("ou=people,o=uni-wuerzburg"));
-//	          //ctx.getAttributes("ou=people,o=uni-wuerzburg").size();
-//	          //ctx.lookup("ou=people,o=uni-wuerzburg");
-//	          //ctx.close();
-//	          LdapConnection.getSearchResult(ctx, "objectClass=*", "ou=people,o=uni-wuerzburg");
-//	          System.out.println(LdapConnection.getSearchResult(ctx, "objectClass=*", "ou=people,o=uni-wuerzburg"));
-//	          System.out.println("erfolg");
-//	      }
-//	      catch (Exception e) {
-//	          System.out.println("misserfolg");
-//	      }
-		
-		
-		
 		Properties env = new Properties();
-	      env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
 
-	      env.put(Context.PROVIDER_URL, "ldaps://auth.uni-wuerzburg.de:636/");//ldap://adressbuch.uni-wuerzburg.de:389/ou=people,o=uni-wuerzburg??one?(objectClass=*)
-	      env.put(Context.SECURITY_PROTOCOL, "ssl");
-	      //env.put(Context.REFERRAL, "follow");
-	      env.put(Context.SECURITY_AUTHENTICATION, "simple");
-	      env.put(Context.SECURITY_PRINCIPAL, "cn=s210507,ou=accounts,o=uni-wuerzburg");
-	      env.put(Context.SECURITY_CREDENTIALS, "artjom84");
-	      try {
-	    	  
-	          DirContext ctx = new InitialLdapContext(env, null);
-	          //System.out.println( ctx.createSubcontext("ou=people,o=uni-wuerzburg"));
-	          //ctx.getAttributes("ou=people,o=uni-wuerzburg").size();
-	          //ctx.lookup("ou=people,o=uni-wuerzburg");
-	          //ctx.close();
-	          LdapConnection.getSearchResult(ctx, "cn=*", "ou=accounts,o=uni-wuerzburg");
-	          //System.out.println(LdapConnection.getSearchResult(ctx, "objectClass=*","ou=accounts,o=uni-wuerzburg"));
-	          System.out.println("erfolg");
-	      }
-	      catch (Exception e) {
-	          System.out.println("misserfolg");
-	      }
-	      
 
+		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+
+//		env.put(Context.PROVIDER_URL, "ldap://adressbuch.uni-wuerzburg.de:389");//ldap://adressbuch.uni-wuerzburg.de:389/ou=people,o=uni-wuerzburg??one?(objectClass=*)
+		env.put(Context.PROVIDER_URL, "ldaps://auth.uni-wuerzburg.de:636/");//ldap://adressbuch.uni-wuerzburg.de:389/ou=people,o=uni-wuerzburg??one?(objectClass=*)
+
+		env.put(Context.SECURITY_PROTOCOL, "ssl");
+		//env.put(Context.REFERRAL, "follow");
+		env.put(Context.SECURITY_AUTHENTICATION, "simple");
+//		env.put(Context.SECURITY_AUTHENTICATION, "none");
+//		//env.put(Context.SECURITY_PRINCIPAL, "ou=people,o=uni-wuerzburg");
+		env.put(Context.SECURITY_PRINCIPAL, "cn=username,ou=accounts,o=uni-wuerzburg");
+		env.put(Context.SECURITY_CREDENTIALS, "password");
+		try {
+
+			DirContext ctx = new InitialLdapContext(env, null);
+			//System.out.println( ctx.createSubcontext("ou=people,o=uni-wuerzburg"));
+			//ctx.getAttributes("ou=people,o=uni-wuerzburg").size();
+			//ctx.lookup("ou=people,o=uni-wuerzburg");
+			//ctx.close();
+			LdapConnection.getSearchResult(ctx, "cn=*", "ou=accounts,o=uni-wuerzburg");
+//			LdapConnection.getSearchResult(ctx, "objectClass=*", "ou=people,o=uni-wuerzburg");
+			//System.out.println(LdapConnection.getSearchResult(ctx, "objectClass=*","ou=accounts,o=uni-wuerzburg"));
+
+			System.out.println("LDAP success");
+		}
+		catch (Exception e) {
+			System.out.println("LDAP failure");
+		}
 
 
 	}
