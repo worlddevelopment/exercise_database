32b1a9fea83196a53ced445e1c69997e9c130372
Jan R.I. B.F. v.W. v.Z. (faerietree) <radagast@ciry.at>
2017-03-10 01:17:27 +0100

    DB: User: Style white space, indent, language. JavaDoc

--- src/db/User.java
+++ src/db/User.java
@@ -1,24 +1,31 @@
 package db;
+
+
 import java.util.Properties;
 
 import javax.naming.directory.DirContext;
 import javax.naming.ldap.InitialLdapContext;
 
-public class User {
-	
-	public String getName(LDAP ldap,String benutzername) {
-		
-		
-		Properties prop = ldap.getProp();
-		
-		try {
-			  
-		     DirContext ctx = new InitialLdapContext(prop, null);
 
-		     return ldap.getSearchResult(ctx, "cn=" + benutzername, "ou=accounts,o=uni-wuerzburg");
-		 }
-		 catch (Exception e) {
-		    return "";
-		 }
+public class User {
+
+	/**
+	 * Get the name of a user as stored in directory accessed via LDAP.
+	 *
+	 * @param ldap An active LDAP wrapper reference.
+	 * @param username The username to retrieve the real name of.
+	 * @return The real name of the user or an empty string.
+	 */
+	public String getName(LDAP ldap, String username) {
+
+		Properties prop = ldap.getProp();
+		try {
+			DirContext ctx = new InitialLdapContext(prop, null);
+			return ldap.getSearchResult(ctx, "cn=" + username
+					, "ou=accounts,o=uni-wuerzburg");
+		}
+		catch (Exception e) {
+			return "";
+		}
 	}
 }
