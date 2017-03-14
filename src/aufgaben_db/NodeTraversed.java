ac53bfc3bdfc7585d8d229bc5240497316372f0c
Jan R.I. B.F. v.W. v.Z. (faerietree) <radagast@ciry.at>
2017-03-14 06:51:04 +0100

    Core: NodeTraversed: Clean up.

--- src/aufgaben_db/NodeTraversed.java
+++ src/aufgaben_db/NodeTraversed.java
@@ -1,47 +1,56 @@
 package aufgaben_db;
 
+
 import org.odftoolkit.odfdom.dom.element.office.OfficeTextElement;
 import org.odftoolkit.odfdom.pkg.OdfFileDom;
 
 
-
 /**
- * For storing a common Node (e.g. org.w3c.dom.Node) and the order index in which place
- * this Node occurred within all Nodes that have been traversed. The nodes traversed 
- * at this point of time are still identical from the beginning on to a certain point/index
- * for each (at this moment still identical) Exercises' XML markup document DOM. (That means
- * the last exercise had to traverse most Nodes as this exercise declaration is the last in the XML).
- * 
- * Exactly spoken it's the sheetdraft's DOM that is being traversed as the initial exercises each are
- * exact copies of the sheetdraft.
- * 
+ * For storing a common Node (e.g. org.w3c.dom.Node) and the order index
+ * in which place this Node occurred within all Nodes that have been
+ * traversed. The nodes traversed at this point of time are still
+ * identical from the beginning on to a certain point/index for each
+ * (at this moment still identical) Exercises' XML markup document DOM.
+ * (That means the last exercise had to traverse most Nodes as this
+ * exercise declaration is the last in the XML).
+ *
+ * Exactly spoken it's the sheetdraft's DOM that is being traversed as
+ * the initial exercises each are exact copies of the sheetdraft.
+ *
  * @reason
- * This was required as elementsTraversed.indexOf() seems not to be able to differentiate between
- * one text:office node and another of the same type (as many of one type/tagname can occur this is
- * a big problem -- e.g. instead of 33 for deepestAllExercisesCommonParent 1 was returned!).
- * @author J. R.I.B.-Wein
+ * This was required as elementsTraversed.indexOf() seems not to be able
+ * to differentiate between one text:office node and another of the same
+ * type. As many of one type/tagname can occur this is a big problem
+ * e.g. instead of 33 for deepestAllExercisesCommonParent 1 was returned!
+ * @author J. R.I.B.-Wein, worlddevelopment
  */
 public class NodeTraversed extends OfficeTextElement {
 
-	
+
+	// ======= ATTRIBUTES
 	private static final long serialVersionUID = 1L;
-	
+
 	private int index_order_position_within_traversed_nodes;
-	
+
 //	private org.w3c.dom.Node node;
-//
-//	
+
+
+
+
+	// ======= CONSTRUCTORS
+	public NodeTraversed(OdfFileDom ownerDoc) {
+		super(ownerDoc);
+	}
+
 //	public NodeTraversed(org.w3c.dom.Node node, int index_order_position_within_traversed_nodes) {
 //		this.node = node;
 //		this.index_order_position_within_traversed_nodes = index_order_position_within_traversed_nodes;
 //	}
 
-	public NodeTraversed(OdfFileDom ownerDoc) {
-		super(ownerDoc);
-	}
 
-	
-	
+
+
+	// ======= METHODS
 	public void setIndex(int index_order_position_within_traversed_nodes) {
 		this.index_order_position_within_traversed_nodes = index_order_position_within_traversed_nodes;
 	}
@@ -51,10 +60,7 @@ public class NodeTraversed extends OfficeTextElement {
 //	public org.w3c.dom.Node getNode() {
 //		return this.node;
 //	}
-	
-	
-	
-	
-	
-	
+
+
+
 }
