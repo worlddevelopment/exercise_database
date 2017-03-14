dcc0ec7bad956e553dd12d474d4f7c0bad3d5b90
Jan R.I. B.F. v.W. v.Z. (faerietree) <radagast@ciry.at>
2017-03-14 06:30:54 +0100

    Core: WordSearchUtility: Clean up code. Style indent,comments,lang
    The usefulness of this utility|file is questionable.

--- src/aufgaben_db/MethodenWortSuche.java
+++ src/aufgaben_db/MethodenWortSuche.java
@@ -1,33 +1,31 @@
 package aufgaben_db;
 
+
 import Verwaltung.HashMapVerwaltung;
 
 
 /**
- * 
- * @author sabine
- * 
- * 
- *  Hier werden v.a. das erste und das letzte Wort im Dokument gesucht
- *  und die dazu passenden Methoden befinden sich hier
+ * Utility for finding the first and last word within a document (body).
+ * TODO The usefulness of this file is questionable.
  *
+ * @author sabine, J.R.I.Balzer-Wein, worlddevelopment
  */
-
-
 public class MethodenWortSuche {
-	
 
-	
+
+
 	/**
-	 * Die Methode ersetzt problematische Java-Sonderzeichen und Umlaute
-	 * durch jeweils ein Leerzeichen (und trennt dadurch auch Woerter!)
-	 * 
+	 * The method replaces problematic (Java?) special chars and Umlauts
+	 * with empty.
+	 * (And thus also splits words!)
+	 * TODO Use Global.encodeUmlauts instead which not loses information.
+	 *
 	 * @changelog J.R.I.B.-Wein put in Unicode representations.
-	 * @param zeile der String, in denen die Zeichen ersetzt werden sollen.
-	 * @return String ohne die boesen Zeichen
+	 * @param zeile The string to replace the chars of
+	 * @return The string without the potentially dangerous characters.
 	 */
 	private static String ersetzeSzUndUmLeer(String zeile) {
-		
+
 		String ergebnis = zeile.replaceAll("\u00D6", " ");
 		//System.out.println(ergebnis);
 		ergebnis = ergebnis.replaceAll("\u00F6", " ");
@@ -46,183 +44,148 @@ public class MethodenWortSuche {
 		//System.out.println(ergebnis);
 		ergebnis = ergebnis.replaceAll("¨"," ");
 		System.out.println(ergebnis);
-		
+
 		return ergebnis;
 	}
-	
- 
+
+
+
 	/**
-	 * Sucht das erste Wort des uebergebenen Dokuments
-	 * fuer die rtf und tex Weiterverarbeitung
-	 * Sonderzeichen und Umlaute fallen dabei weg
-	 * 
-	 * @param filename String[] des zu untersuchenden Textes (Zeilenweise)
-	 * @return String des ersten Wortes
+	 * Searches the first word of the given document contents
+	 * for continued processing of RTF, TEX.
+	 * FIXME Special chars and Umlauts are lost. TODO Encode.
+	 *
+	 * @param filename Array of lines of the file's contents.
+	 * @return The first word of the document (body) or "" if none.
 	 */
 	public static String sucheErstesWortImDoc(String[] filename) {
-		
-		System.out.println("Suche erstes Wort im Dokument wurde aufgerufen");
-		String ergebnis = "";
-		boolean erfolgreich = false;
-		
+
+		System.out.println("Searching 1st word in document.");
+
 		for (int i = 0; i < filename.length; i++) {
-			System.out.println("Zeile: "+i);
-			if (filename[i].length() > 3 ) { // Woerter < 4 Buchstaben werden sowieso nicht gewertet
-				// alle SZ und Umlaute werden durch Leerezeichen ersetzt
-				String zeile = ersetzeSzUndUmLeer(filename[i]);
-				System.out.println(zeile);
-				// jetzt wird die Zeile in 'Woerter'(=Wortbruchstuecke) aufgeteilt
-				String[] array = teileZeileInWoerter(zeile);
-				
-				// das erste Wortfragment, welches aus mind. 4 zusammenhaengenden
-				// Zeichen besteht, wird ausgewaehlt:
-				
-				// 4 ist jetzt reine Abschaetzung, hier laesst sich noch was veraendern
-				// durch Betrachtung der ersten Worte entstanden
-				for (int j = 0; j < array.length; j++) {
-					System.out.println("Laenge des Wortes:"+ array[j].length());
-					if (array[j].length() >=4) {
-						System.out.println("Eingetragen!");
-						ergebnis = array[j];
-						System.out.println(ergebnis);
-						erfolgreich = true;
-						if (erfolgreich) {
-							return ergebnis;
-							}
-					}
+			System.out.println("Line: " + i);
+
+			// Words with letter count < 4 are ignored:
+			// FIXME 4 is hardcoded, guessed
+			if (filename[i].length() < 4 ) {
+				continue;
+			}
+
+			String zeile = ersetzeSzUndUmLeer(filename[i]);
+			System.out.println(zeile);
+			// Split into words|word chunks:
+			String[] array = zeile.split("\\s");
+
+			// First word fragment made of > 3 connected chars:
+			for (int j = 0; j < array.length; j++) {
+				String w = array[j];
+				if (w.length() < 4) {
+					continue;
 				}
-				
-				
-				// falls in der Zeile kein passendes Wort enthalten war
-				// wird in der n\u00E4chsten Zeile weitergesucht
+				System.out.println("Word length:" + w.length());
+				System.out.println("Found first word: " + w);
+				return w;
+			}
+
+		}
+		// => No word found in Document.
+		return "";
+	}
+
+
+
+	/**
+	 * Search the last content word of document.
+	 *
+	 * @param filename Lines of raw content.
+	 * @return The last word or "".
+	 */
+	public static String sucheLetztesWortImDoc(String[] raw_content) {
+
+		for (int i = raw_content.length - 1; i > -1; i--) {
+			String line = raw_content[i];
+			// Words with letter count < 2 are ignored: FIXME hardcoded
+			if (line.length() < 2) {
+				continue;
+			}
+
+			String zeile = ersetzeSzUndUmLeer(line);
+			// Split into words|word chunks:
+			String[] array = zeile.split("\\s");
+
+			for (int j = array.length - 1; j > -1; j--) {
+				String w = array[j];
+				if (w.length() < 2) {
+					continue;
+				}
+				System.out.println("Word length:" + w.length());
+				System.out.println("Found last word: " + w);
+				return w;
 			}
 		}
-		// falls sich im ganzen Dokument kein Wort fand, wird der leere String zurueckgegeben
-		return ergebnis;
-	}
-	
- 
-	
-	/**
-	 * Sucht das letzte Wort des uebergebenen Dokuments
-	 * fuer die rtf und tex Weiterverarbeitung
-	 * Sonderzeichen und Umlaute fallen dabei weg
-	 * 
-	 * @param filename String[] des zu untersuchenden Textes (Zeilenweise)
-	 * @return String des letzten Wortes
-	 */
-	public static String sucheLetztesWortImDoc(String[] filename) {
-		
-		String ergebnis = "";
-		
-		for (int i = filename.length-1; i >= 0; i--) {
-			if (filename[i].length() > 1 ) { // Woerter < 2 Buchstaben werden sowieso nicht gewertet
-				// alle SZ und Umlaute werden durch Leerezeichen ersetzt
-				String zeile = ersetzeSzUndUmLeer(filename[i]);
-				// jetzt wird die Zeile in 'Woerter'(=Wortbruchstuecke) aufgeteilt
-				String[] array = teileZeileInWoerter(zeile);
-				
-				// das letzte Wortfragment, welches aus mind. 2 zusammenhaengenden
-				// Zeichen besteht, wird ausgewaehlt:
-				for (int j = array.length - 1; j >= 0; j--) {
-					if (array[j].length() >=2) {
-						ergebnis = array[j];
-						return ergebnis;
-					}
-				// falls in der Zeile kein passendes Wort enthalten war
-				// wird in der n\u00E4chsten Zeile weitergesucht
-				}
-			}
-		}
-		// falls sich im ganzen Dokument kein Wort fand, wird der leere String zurueckgegeben
-		return ergebnis;
-	}
-	
-	
-	
-	// teilt eine Zeile in einzelne Woerter auf
-	
-	/**
-	 * Teilt eine Zeile in ihre einzelnen, durch Leerzeichen getrennten Woerter auf
-	 * 
-	 * @param zeile String der aufzuteilenden Zeile
-	 * @return String[] der aufgeteiteilten Zeile (Ein Wort pro Array-Eintrag)
-	 */
-	public static String[] teileZeileInWoerter(String zeile) {
-		//System.out.println("teile Zeile in Woerter wurde aufgerufen");
-		
-		String[] result = zeile.split("\\s");
+		// => No word in document!
+		return "";
 
-		//System.out.println("teile Zeile in Woerter wurde beendet");
-
-		return result;
 	}
-	
-	
-	
-	//2 Methoden zum Kuerzen von Arrays auf eine vorgegebene Laenge:
-	
-	
-	// kuerzt Arrays auf die richtige Laenge
-	
+
+
+
 	/**
-	 * Kuerzt die Laenge des uebergebenen String[] auf den Wert zaehler
-	 * 
-	 * @param array zu kuerzendes String[]
-	 * @param zaehler Laenge, auf die das String[] gekuerzt werden soll
-	 * @return gekuerztes String[]
+	 * Shorten the given array to a given length. (removing from tail)
+	 * TODO The usefulness of this method is questionable.
+	 *
+	 * @param array to shorten.
+	 * @param zaehler Length to shorten to.
+	 * @return The shortened array.
 	 */
 	public static String[] kuerzeArray(String[] array, int zaehler) {
-		//System.out.println("kuerzeArray String[] wurde aufgerufen");
+		//System.out.println("kuerzeArray ...");
 		String[] ergebnis  = new String[zaehler];
 		for (int i = 0; i < zaehler; i++) {
-			ergebnis[i] = array[i]; 
-		}
-		//System.out.println("kuerzeArray String[] wurde beendet");
-		return ergebnis;
-	}
-	
-
-	
-	// kuerzt zweidimensionale arrays auf die richtige Laenge
-	
-	/**
-	 * Kuerzt die Laenge des uebergebenen String[][] auf den Wert zaehler
-	 * 
-	 * @param array zu kuerzendes String[][]
-	 * @param zaehler Laenge, auf die das String[][] gekuerzt werden soll
-	 * @return gekuerztes String[]
-	 */
-	public static String[][] kuerzeArray(String[][] array, int zaehler) {
-		// die 2 stimmt ev. noch nicht.....:
-		String[][] ergebnis  = new String[zaehler][2];
-		for (int i = 0; i < zaehler; i++) {
-			//ergebnis[i][0] = array[i][0]; 
-			//ergebnis[i][1] = array[i][1];
-			// so scheint es auch zu gehen:
 			ergebnis[i] = array[i];
 		}
 		return ergebnis;
 	}
-	
+
+
+
 	/**
-	 * Schreibt das erste und letzte Wort des uebergebenen String[] in die Hashmap
-	 * 
-	 * @param textdoc zeilenweise gespiecherter Text als String[]
+	 * Shorten 2 dimensional arrays.
+	 * TODO The usefulness of this method is questionable.
+	 *
+	 * @param array to shorten.
+	 * @param zaehler Length to shorten to.
+	 * @return The shortened 2 dimensional array.
 	 */
-	public static void schreibeErstesLetztesWortAusTextInHashMap(String[] textdoc) {
-		
-		String erstesWort = MethodenWortSuche.sucheErstesWortImDoc(textdoc);
-		System.out.println("Das erste Wort des Dokuments lautet: " + erstesWort);
-		
-		HashMapVerwaltung.erweitereHashmapString(HashMapVerwaltung.docAnf, erstesWort);
-		
-		
-		String letztesWort = MethodenWortSuche.sucheLetztesWortImDoc(textdoc);
-		System.out.println("Das letzte Wort des Dokuments lautet: " + letztesWort);
-		
-		HashMapVerwaltung.erweitereHashmapString(HashMapVerwaltung.docEnde, letztesWort);
+	public static String[][] kuerzeArray(String[][] array, int zaehler) {
+		// FIXME The 2 is a magic number, no reason|origin given.
+		String[][] ergebnis  = new String[zaehler][2];
+		for (int i = 0; i < zaehler; i++) {
+			ergebnis[i] = array[i];
+		}
+		return ergebnis;
 	}
-	
+
+
+
+	/**
+	 * Writes the first and last word of content into a HashMap.
+	 *
+	 * @param textdoc Array of lines of raw content.
+	 */
+	public static void schreibeErstesLetztesWortAusTextInHashMap(
+			String[] textdoc) {
+
+		String erstesWort = MethodenWortSuche
+			.sucheErstesWortImDoc(textdoc);
+		HashMapVerwaltung.erweitereHashmapString(
+				HashMapVerwaltung.docAnf, erstesWort);
+
+		String letztesWort = MethodenWortSuche
+			.sucheLetztesWortImDoc(textdoc);
+		HashMapVerwaltung.erweitereHashmapString(
+				HashMapVerwaltung.docEnde, letztesWort);
+	}
+
 
 }
