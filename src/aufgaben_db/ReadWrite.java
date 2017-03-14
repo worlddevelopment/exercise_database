206bf2f41d85be9730920d6d61fc2bfc19327883
Jan R.I. B.F. v.W. v.Z. (faerietree) <radagast@ciry.at>
2017-03-14 07:44:50 +0100

    Core: ReadWrite: Clean up white space,indent,comments,language.

--- src/aufgaben_db/ReadWrite.java
+++ src/aufgaben_db/ReadWrite.java
@@ -1,5 +1,6 @@
 package aufgaben_db;
 
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
@@ -24,191 +25,210 @@ import java.util.ArrayList;
 
 
 /**
- * 
- * @author sabine
- * @author J.R.I.B.-W. complete overhaul.
- * 
- * Die Klasse dient als Utility-Klasse der Kategorie IO/Read&Write.
- * Textdateien k\u00F6nnen eingelesen oder zur Festplatte geschrieben werden.
- * 
- * Beim Einlesen erhaelt man als Rueckgabe jeweils String[];
+ * Utility for IO | read, write.
+ * TODO Rename to IO?
+ *
+ * @author sabine, J.R.I.B.-W., worlddevelopment
  */
-
-
-
 public class ReadWrite {
-	
+
 	/**
-	 * 
-	 * Liest eine Textdatei ein und gibt diese Zeilenweise als String-Array zurueck
-	 * 
-	 * @param filename Absoluter Pfad der Datei
-	 * @return String-Array mit den Zeilen der Textdatei als Inhalt
-	 * @throws FileNotFoundException Wenn die angegebene Datei nicht vorhanden ist.
+	 * Reads a text file from disk into an array of lines.
+	 *
+	 * @param filename Absolute path to file to read.
+	 * @return Array of lines of the raw content.
+	 * @throws FileNotFoundException If the file not exists.
 	 */
-	public static String[] loadText(String filename) throws FileNotFoundException {
-		
-		BufferedReader reader = new BufferedReader(new FileReader(filename));
-		System.out.println("Das File " + filename + " wurde erfolgreich eingelesen.");
-        return readInputStreamAsLines(reader);
-        
-    }
-	
-	
-	
-	
-	
+	public static String[] loadText(String filename)
+		throws FileNotFoundException {
+
+		BufferedReader reader
+			= new BufferedReader(new FileReader(filename));
+		System.out.println("Read file from disk: " + filename);
+		return readInputStreamAsLines(reader);
+
+	}
+
+
+
 	/**
 	 * Write 'Exercise Raw Content' to disk.
+	 *
 	 * @param exercise
 	 * @throws IOException
 	 */
 	//static void writeRawContentToDiskDependingOnEnding(Exercise exercise) throws IOException {
-	static void writeRawContentToDiskDependingOnEnding(Exercise exercise) throws IOException {
-		/* At this point we have the rough idea of an exercise and want
-		 * to create it on the harddisk. The idea is to copy the complete
-		 * mother sheet docx/zip/tex to a new filename in the same directory
-		 * and to delete the parts not belonging to the exercise we want.
-		 */
+	static void writeRawContentToDiskDependingOnEnding(Exercise exercise)
+		throws IOException {
+
+		/*
+		At this point we have a rough idea of an exercise
+		and want to create it on the harddisk.
+		The idea is to copy the complete 'mother' sheet docx/zip/tex
+		to a new filename in the same directory and to delete
+		the parts not belonging to the exercise.
+		*/
 		String ext = Global.extractEnding(exercise.filelink);
-		//MICROSOFT
+
+		// DOCX
 		if (ext.equals("docx")) {
-			//1) determine mother sheet to this exercise
+			// 1) determine mother sheet to this exercise
 			String motherfilelink = Global.extractFilelinkOfMothersheet(exercise.filelink);
 			File mothersheetToExerciseFile = new File(motherfilelink);
 			if (!mothersheetToExerciseFile.exists()) {
-				Global.addMessage("Warning: No mother sheet '" + motherfilelink + "' found for the"
-							+ " given exercise: '" + exercise.filelink + "' ! => So no copy can be created"
-							+ " to obtain the exercise by deleting obsolete (for the specific exercise) conten.", "warning");
+				Global.addMessage("Warning: No mother sheet '"
+						+ motherfilelink + "' found for the"
+						+ " given exercise: '" + exercise.filelink
+						+ "' ! => So no copy can be created"
+						+ " to obtain the exercise by deleting"
+						+ " obsolete (for the specific exercise)"
+						+ " conten.", "warning");
 				//mothersheetToExerciseFile.mkdirs();
 				return;
 			}
-			
-			//2) copy this (mother) sheet's docx/zip completely to new exercise.filelink.docx
-			//needs NIO - NewIO - delivered with JDK7
-//			Files.copy(targetFilelink, exercise.filelink, REPLACE_EXISTING/*NOFOLLOW_LINKS*/);
+
+			// 2) copy this sheet's docx to new exercise.filelink.docx
+			// Requires NIO - NewIO - delivered with JDK7:
+//			Files.copy(targetFilelink, exercise.filelink
+//					, REPLACE_EXISTING/*NOFOLLOW_LINKS*/);
 			File exerciseFilelinkFile = new File(exercise.filelink);
 			if (exerciseFilelinkFile.exists()) {
-				Global.addMessage("Warning: Exercise '" + exercise.filelink + " to create"
-							+ " already exists.", "warning");
-				//overwrite it?? TODO
-				return;//for now better exit, no overwritten files so far!!
-			} else {
-				//
-				Global.addMessage("Important: If the exercise files are to be stored in a different folder then make sure the next line is uncommented. (see source code)", "info");
+				Global.addMessage("Warning: Exercise '"
+						+ exercise.filelink + " to create"
+						+ " already exists.", "warning");
+				// For now do not overwrite any files:TODO Overwrite it?
+				return;
+			}
+			else {
+				Global.addMessage("Creating directories for storing"
+						+ " files of content parts.", "info");
 				if (exerciseFilelinkFile.mkdirs()) {
-					Global.addMessage("--> No folder had to be created ...", "success");
+					Global.addMessage("- No folder had to be created."
+							, "success");
 				}
 			}
-			Global.addMessage("Copying mother sheet file to exercise destination filelink.", "info");
-			copyFileUsingStream(mothersheetToExerciseFile, exerciseFilelinkFile);
+			Global.addMessage("Copying mother sheet file to exercise"
+					+ " destination filelink.", "info");
+			copyFileUsingStream(mothersheetToExerciseFile
+					, exerciseFilelinkFile);
 
-			
-			//3) Open this newly created archive and delete content that's not needed.
-			//   e.g. replace the document.xml-Body with the extracted exercise rawContent.
-			//        KEEP HEADERS!
-			//        Delete line per line,
-			//        follow references, delete them if no more any references too.
-			//TODO using docx4j
-			
-			
-			
-		}	
+			// 3) Open this newly created archive and delete content
+			// that's not needed.
+			// e.g. replace the document.xml-Body with the extracted
+			// exercise rawContent and
+			// * KEEP HEADERS!
+			// * Delete line per line,
+			// * follow references, delete those no longer in use.
+			// Note this is an early outline before it was
+			// coded in Exercise.
+
+
+		}
 		else if (ext.equals("doc")) {
-			
-		}
-		
-		//OPEN / LIBRE OFFICE
-		else if (ext.equals("ods")) {
-			
+
 		}
 
-		
-		//The general case: i.e. for all SINGLE-FILE AND NON-ARCHIVE DATATYPES!!!
+
+		// OPEN / LIBRE OFFICE
+		else if (ext.equals("ods")) {
+
+		}
+
+
+		// GENERAL CASE: single file, non-archive
 		else {
-			//Simply write it to a single file for: PLAIN TEXT, PDF
+			// Write it to a single file for: PLAIN TEXT, PDF
 			ReadWrite.writeText(
 					readInputStreamAsLines(exercise.filelink)
-					, exercise.filelink.replaceAll(Global.extractEnding(exercise.filelink) + "$", "txt")
+					, exercise.filelink.replaceAll(
+						Global.extractEnding(exercise.filelink)
+						+ "$", "txt")
 			);
 		}
-		
-				
+
+
 	}
-	
-	
-	
-	
-	
-	//READ INPUT STREAM AS LINES
-	public static String[] readInputStreamAsLines(String  filelink) throws IOException {
-		return readInputStreamAsLines(Global.getInputStreamDependingOnEnding(filelink));
+
+
+
+	/**
+	 * READ INPUT STREAM AS LINES
+	 *
+	 * @param filelink
+	 */
+	public static String[] readInputStreamAsLines(String filelink)
+		throws IOException {
+		return readInputStreamAsLines(
+				Global.getInputStreamDependingOnEnding(filelink));
 	}
-	
-	//READ FROM STREAM
-	public static String[] readInputStreamAsLines(InputStream inputStream) {
-		//http://stackoverflow.com/questions/4473256/reading-text-files-in-a-zip-archive
-		//To decode from binary to text:
-		InputStreamReader inputStreamReader = new InputStreamReader(inputStream); 
-		//To load one line at a time:
+
+
+
+	/**
+	 * @param inputStream
+	 */
+	public static String[] readInputStreamAsLines(
+			InputStream inputStream) {
+
+		// http://stackoverflow.com/questions/4473256/reading-text-files-in-a-zip-archive
+		// To decode from binary to text:
+		InputStreamReader inputStreamReader
+			= new InputStreamReader(inputStream);
+		// To load one line at a time:
 		BufferedReader reader = new BufferedReader(inputStreamReader);
 		return readInputStreamAsLines(reader);
+
 	}
+
+
+
 	public static String[] readInputStreamAsLines(BufferedReader reader) {
-		
+
 		String zeile = null;
 		ArrayList<String> lines = new ArrayList<String>();
 		String[] values = null;
 		//System.out.println(zeile);
 		try {
 			while((zeile = reader.readLine()) != null) {
-			    lines.add(zeile);
-			    //System.out.println(i + ": " + zeile);
+				lines.add(zeile);
+				//System.out.println(i + ": " + zeile);
 			}
 		} catch (IOException e1) {
 			e1.printStackTrace();
-			
+
 		}
 		try {
 			reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-        values = new String[lines.size()];
-        values = lines.toArray(values);
+		values = new String[lines.size()];
+		values = lines.toArray(values);
 
-        return values;
-    }
-	
-	
-	
-	
-	
-	
-	
-	
+		return values;
+	}
 
-	// Methode zum Erstellen von Text Files,
-	// Wird sowohl fuer das Textfile als auch
-	// fuer die einzelnen Aufgabebloecke
-	// sowohl im .txt als auch im .rtf-Format verwendet 
-	// Eingabe: textfile: file, welches erstellt werden soll
-	// filename: so soll es dann genannt werden (mit .txt bzw. .rtf)
-	
+
+
 	/**
-	 * 
-	 * Methode zum Erstellen von Text Files, wird sowohl fuer das Textfile, als auch fuer die einzelnen Aufgabenbl�cke
-	 * sowohl im .txt. als auch im .rtf-Format verwendet
-	 * 
-	 * @param text file, welches erstellt werden soll
-	 * @param filelink so soll es dann genannt werden (mit .txt bzw. .rtf)
-	 * @throws IOException 
+	 *
+	 * Writes text to file system.
+	 * Use for text based formats like plain text .txt or markup like
+	 * e.g. .rtf, .md, ...
+	 *
+	 * @param text array of string to write to file.
+	 * @param filelink including ending.
+	 * @throws IOException
 	 */
-	public static void writeText(String[] text, String filelink) throws IOException {
-		//writeText(new BufferedWriter(new FileWriter(filelink)), filelink);
-		//System.out.println("Jetzt wird der Text in die neue Datei geschrieben!");
+	public static void writeText(String[] text, String filelink)
+		throws IOException {
+
+		//writeText(new BufferedWriter(new FileWriter(filelink))
+		//				, filelink);
+		//System.out.println("Trying to write text to file.");
 		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter(filelink));
+			BufferedWriter bw
+				= new BufferedWriter(new FileWriter(filelink));
 			for (int i = 0; i < text.length; i++) {
 //				System.out.println(i);
 				if (text[i] != null) {
@@ -216,26 +236,35 @@ public class ReadWrite {
 					bw.newLine();
 				}
 			}
-			System.out.println("Die neue Datei : " + filelink + " wurde erstellt.");
+			System.out.println("Create file: " + filelink);
 			bw.close();
 		}
-		catch (FileNotFoundException e) { e.printStackTrace(); }
-		catch (IOException e) { e.printStackTrace(); }
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	
-	
+
+
+
 	/**
-	 * File copy class using the quickest possibility available in JAVA until now for big files.
+	 * File copy class using the quickest possibility available
+	 * in Java until now for big files.
 	 * (http://www.journaldev.com/861/4-ways-to-copy-file-in-java)
+	 *
 	 * @param source
 	 * @param destination
 	 * @throws IOException
 	 */
-	public static void copyFileUsingStream(File source, File destination) throws IOException {
+	public static void copyFileUsingStream(File source
+			, File destination) throws IOException {
+
 		InputStream is = null;
 		OutputStream os = null;
-		//To not write bytewise or at least keep the conversion of the string os-independent, use e.g. PrintWriter.  
+		// To not write bytewise or at least keep the conversion
+		// of the string os-independent, use e.g. PrintWriter.
 		try {
 			is = new FileInputStream(source);
 			os = new FileOutputStream(destination);
@@ -249,40 +278,46 @@ public class ReadWrite {
 			is.close();
 			os.close();
 		}
+
 	}
-	
-	
-	
+
+
+
 	/**
-	 * WRITE
-	 * Text, binary currently not supported.
-	 * (http://stackoverflow.com/questions/2885173/java-how-to-create-and-write-to-a-file)
-	 * @param string    - The text to write to file.
-	 * @param filelink  - The destination file. Will be created or overwritten.
-	 * @param overwrite - Unless explicitely stated not to overwrite (default: true).
+	 * WRITE text.
+	 * TODO Support binary.
+	 * http://stackoverflow.com/questions/2885173/java-how-to-create-and-write-to-a-file
+	 *
+	 * @param string The text to write to file.
+	 * @param filelink The destination file.
+	 * @param overwrite Set to true by default.
 	 */
 	public static void write(String[] lines, String filelink) {
 		String linesAsString = Global.arrayToString(lines);
 		write(linesAsString, filelink);
 	}
-	
+
 	public static void write(String string, String filelink) {
 		write(string, filelink, false);
 	}
-	public static void write(String string, String filelink, boolean isAppended) {
-		
+
+	public static void write(String string
+			, String filelink, boolean isAppended) {
+
 //		ONLY WORKS IN JAVA 1.7 or if NEW IO (nio) package is available.
 //		Charset charset = Charset.forName("UTF-8");
-//		try (BufferedWriter writer = Files.newBufferedWriter(file, charset)) {
+//		try (BufferedWriter writer
+//				= Files.newBufferedWriter(file, charset)) {
 //			writer.write(string, 0, string.length());
 //		} catch (IOException x) {
 //			System.err.format("IOException: %s%n", x);
 //		}
-		
+
 		Writer writer = null;
 		try {
 			if (isAppended) {
-				writer = new BufferedWriter(new FileWriter(filelink, isAppended));
+				writer = new BufferedWriter(
+						new FileWriter(filelink, isAppended));
 			}
 			else {
 				writer = new BufferedWriter(
@@ -297,31 +332,36 @@ public class ReadWrite {
 		catch (IOException e) {
 			System.err.format("IOException: %s%n .", e);
 			e.printStackTrace();
-			Global.addMessage("Failed: Writing binary/text to file: " + filelink, "danger");
+			Global.addMessage("Failed: Writing binary/text to file: "
+					+ filelink, "danger");
 		}
 		finally {
-			try { writer.close(); } catch (Exception e) { e.printStackTrace(); }
+			try {
+				writer.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		
-		
-//      //The non-proper way as this is for 
-//		PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
+
+
+//		PrintWriter writer
+//			= new PrintWriter("the-file-name.txt", "UTF-8");
 //		writer.println("The first line");
 //		writer.println("The second line");
 //		writer.close();
 //
-//		//Creating a binary file (will also overwrite the file):
+//		// Creating a binary file (will also overwrite the file):
 //
 //		byte dataToWrite[] = //...
 //		FileOutputStream out = new FileOutputStream("the-file-name");
 //		out.write(dataToWrite);
 //		out.close();
-		
-		
+
+
 	}
 
-	
-	
-	
-	
+
+
+
+
 }
